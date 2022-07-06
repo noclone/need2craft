d3d2b3af5af293e15885df6696e125b1b6f3b439
Jean Fechter <jean.fechter@epita.fr>
2022-07-07 00:31:17 +0200

    newcraft restyled

--- src/components/pages/NewCraftPage.js
+++ src/components/pages/NewCraftPage.js
@@ -1,20 +1,24 @@
 import ItemsList from "../ItemsList";
-import NewItem from "../NewItem"
-import Craft from "../Craft"
+import NewItem from "../NewItem";
+import Craft from "../Craft";
 
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-function NewCraftPage(props){
-    return (
+function NewCraftPage(props) {
+  return (
     <DndProvider backend={HTML5Backend}>
-        <div className="craftOverlay">
-          <Craft loggedIn={props.loggedIn}/>
-          <ItemsList loggedIn={props.loggedIn}/>
-          <NewItem loggedIn={props.loggedIn}/>
+      <div className="craftOverlay">
+        <div>
+          <Craft loggedIn={props.loggedIn} />
+          <ItemsList loggedIn={props.loggedIn} />
         </div>
+        <div>
+          <NewItem loggedIn={props.loggedIn} />
+        </div>
+      </div>
     </DndProvider>
-    );
+  );
 }
 
 export default NewCraftPage;
